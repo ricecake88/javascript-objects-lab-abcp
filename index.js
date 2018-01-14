b1b76c145ccc4fea5fab7992a117d423e6d8ed67
Grace Shih <vocalise@gmail.com>
2018-01-14 10:34:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function deleteFromObjectByKey(obj, name) {
   delete newObj[name];
   return newObj;
 }
+
+function destructivelyDeleteFromObjectByKey(obj, name) {
+  delete obj[name];
+  return obj;
+}
